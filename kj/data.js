fb203b9e2c952ebecf3b9ab72704ecd3b69b8dac
tangyihang <tangyihangchina@163.com>
2018-10-27 20:20:26 +0800

    修改开奖导致的bug

--- kj/data.js
+++ kj/data.js
@@ -197,7 +197,7 @@ function submitData(data, conf) {
       return ;
     }
     // 澳门快三检查
-    if (result.length && result[0].type === 63 ) {
+    if ( result[0].type === 63 && result.length ) {
       data = result[0];
       // 获取开奖信息
       calcJ(data, true);
